f767ad297747c319f0b6ad3c11038720b9ba59c2
daviderocco85 <roccodavide85@gmail.com>
2026-09-24 16:29:13 +0200

    refactor: modularize pizzas routing and error handling

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 import express from 'express';
-import { connection } from './config/db.js';
+import { pizzasRouter } from './routers/pizzasRouter.js';
 
 const app = express();
 const port = 3000;
 
-app.get('/pizzas', async (req, res) => {
-    try {
-        const sql = 'SELECT * FROM pizzas';
-        const [results] = await connection.query(sql);
+app.use('/pizzas', pizzasRouter);
 
-        res.json(results);
-    }
-    catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Something went wrong with the sql query' });
-    }
+app.use((err, req, res, next) => {
+
+    console.error(err);
+    res.status(500).json({ error: 'Something went wrong with the sql query' });
+
 });
 
 app.listen(port, () => {
