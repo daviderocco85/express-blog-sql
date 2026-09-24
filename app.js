1991d78a31f2665ad0d9d55bee52fa0444ab48ef
daviderocco85 <roccodavide85@gmail.com>
2026-09-24 15:36:48 +0200

    feat: return pizzas list from MySQL database

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 import express from 'express';
+import { connection } from './config/db.js';
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/pizzas', async (req, res) => {
+    try {
+        const sql = 'SELECT * FROM pizzas';
+        const [results] = await connection.query(sql);
+
+        res.json(results);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Something went wrong with the sql query' });
+    }
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
