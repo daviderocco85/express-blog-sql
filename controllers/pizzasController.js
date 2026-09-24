e2e5b250535b44d9c05cf3ea2086581457b761bb
daviderocco85 <roccodavide85@gmail.com>
2026-09-24 17:40:34 +0200

    feat: include ingredients in pizza details

--- controllers/pizzasController.js
+++ controllers/pizzasController.js
@@ -18,15 +18,25 @@ export const getById = async (req, res) => {
         return;
     }
 
-    const sql = 'SELECT * FROM pizzas WHERE id = ?';
-    const [[result]] = await connection.query(sql, [id]);
+    const sqlPizza = 'SELECT * FROM pizzas WHERE id = ?';
+    const [[resultPizza]] = await connection.query(sqlPizza, [id]);
 
-    if (result === undefined) {
+    if (resultPizza === undefined) {
         res.status(404).json({ error: 'pizza not found' });
         return;
     }
 
-    res.json(result);
+    const sqlIngredients = `
+        SELECT i.name
+        FROM ingredients i
+        JOIN ingredient_pizza ip ON ip.ingredient_id = i.id
+        WHERE ip.pizza_id = ?
+      `;
+
+    const [resultIngredients] = await connection.query(sqlIngredients, [id]);
+    resultPizza.ingredients = resultIngredients.map(i => i.name);
+
+    res.json(resultPizza);
 };
 
 export const destroyById = async (req, res) => {
