4c6fa1db10a29f4b14b11c35be4296fba03c6281
daviderocco85 <roccodavide85@gmail.com>
2026-09-24 14:53:40 +0200

    feat: add MySQL database connection configuration

---
+++ config/db.js
@@ -0,0 +1,10 @@
+import mysql from 'mysql2/promise';
+
+export const connection = await mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'davide85',
+    database: 'pizzeria_db'
+});
+
