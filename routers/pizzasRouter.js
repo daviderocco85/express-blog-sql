294b7247b5679ab367f9d456321f53f1a1a2c338
daviderocco85 <roccodavide85@gmail.com>
2026-09-24 17:11:06 +0200

    feat: add getById route to pizzas router

--- routers/pizzasRouter.js
+++ routers/pizzasRouter.js
@@ -6,6 +6,9 @@ export const pizzasRouter = express.Router();
 // Index di tutte le pizze
 pizzasRouter.get('/', pizzasController.getAll);
 
+// Show dell'articolo con specifico id
+pizzasRouter.get('/:id', pizzasController.getById);
+
 // Destroy
 pizzasRouter.delete('/:id', pizzasController.destroyById);
 
