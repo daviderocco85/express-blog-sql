326af8468ea1db01628617d4686d646df6b886c9
daviderocco85 <roccodavide85@gmail.com>
2026-09-25 13:54:09 +0200

    refactor: update route from pizzas to posts and message error in global middleware

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import express from 'express';
-import { pizzasRouter } from './routers/pizzasRouter.js';
+import { postsRouter } from './routers/postsRouter.js';
 
 const app = express();
 const port = 3000;
 
-app.use('/pizzas', pizzasRouter);
+app.use('/posts', postsRouter);
 
 app.use((err, req, res, next) => {
 
     console.error(err);
-    res.status(500).json({ error: 'Something went wrong with the sql query' });
+    res.status(500).json({ error: 'Something went wrong, internal server error' });
 
 });
 
