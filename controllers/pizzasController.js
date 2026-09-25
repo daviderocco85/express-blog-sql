298be19d4cf2b4b8dd162a8d443f5e879778a007
daviderocco85 <roccodavide85@gmail.com>
2026-09-25 13:29:24 +0200

    refactor: update controller variables and queries for blog DB

--- controllers/pizzasController.js
+++ controllers/pizzasController.js
@@ -2,7 +2,7 @@ import { connection } from '../config/db.js';
 
 export const getAll = async (req, res) => {
 
-    const sql = 'SELECT * FROM pizzas';
+    const sql = 'SELECT * FROM posts';
     const [results] = await connection.query(sql);
 
     res.json(results);
@@ -14,40 +14,40 @@ export const getById = async (req, res) => {
     const id = Number(req.params.id);
 
     if (Number.isNaN(id) || !Number.isInteger(id)) {
-        res.status(400).json({ error: 'Id must be an integer' });
+        res.status(400).json({ error: 'Id deve essere un numero intero' });
         return;
     }
 
-    const sqlPizza = 'SELECT * FROM pizzas WHERE id = ?';
-    const [[resultPizza]] = await connection.query(sqlPizza, [id]);
+    const sqlPost = 'SELECT * FROM posts WHERE id = ?';
+    const [[resultPost]] = await connection.query(sqlPost, [id]);
 
-    if (resultPizza === undefined) {
-        res.status(404).json({ error: 'pizza not found' });
+    if (resultPost === undefined) {
+        res.status(404).json({ error: 'Post non trovato' });
         return;
     }
 
-    const sqlIngredients = `
-        SELECT i.name
-        FROM ingredients i
-        JOIN ingredient_pizza ip ON ip.ingredient_id = i.id
-        WHERE ip.pizza_id = ?
+    const sqlTags = `
+        SELECT t.label
+        FROM tags t
+        JOIN post_tag pt ON pt.tag_id = t.id
+        WHERE pt.post_id = ?
       `;
 
-    const [resultIngredients] = await connection.query(sqlIngredients, [id]);
-    resultPizza.ingredients = resultIngredients.map(i => i.name);
+    const [resultTags] = await connection.query(sqlTags, [id]);
+    resultPost.tags = resultTags.map(t => t.label);
 
-    res.json(resultPizza);
+    res.json(resultPost);
 };
 
 export const destroyById = async (req, res) => {
     const id = Number(req.params.id);
 
     if (Number.isNaN(id) || !Number.isInteger(id)) {
-        res.status(400).json({ error: 'Id must be an integer' });
+        res.status(400).json({ error: 'Id deve essere un numero intero' });
         return;
     }
 
-    const sql = 'DELETE FROM pizzas WHERE id = ?';
+    const sql = 'DELETE FROM posts WHERE id = ?';
     await connection.query(sql, [id]);
 
 
